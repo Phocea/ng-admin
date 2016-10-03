bed854199f72aeadadc489e0208be0767eee0c13
Phocea <phocea04500@hotmail.com>
2016-10-03 16:52:47 +0200

    Update ErrorHandler.js

--- src/javascripts/ng-admin/Main/run/ErrorHandler.js
+++ src/javascripts/ng-admin/Main/run/ErrorHandler.js
@@ -3,6 +3,7 @@ export default function errorHandler($rootScope, $state, $translate, notificatio
         if (error.status == 404) {
             $state.go('ma-404');
             event.preventDefault();
+            
         } else {
             $translate('STATE_CHANGE_ERROR', { message: error.message }).then(text => notification.log(text, { addnCls: 'humane-flatty-error' }));
             throw error;
